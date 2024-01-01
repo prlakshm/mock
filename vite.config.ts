9c94bd15e4ea45fd12d92988b6b5163ad86be48c
prlakshm <pranavi_lakshminarayanan@brown.edu>
2023-12-31 20:54:44 -0500

    Fix: updated build directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: '/mock-prlakshm-dkyerema-prlakshm-deploy/',
+    base: '/mock',
     build: {
       outDir: 'build',
     },
